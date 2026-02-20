4d2cdca4bed78efbb818afc13a06a7e376f9db96
Angélica <angelik797@icloud.com>
2026-02-20 16:57:23 -0500

    update cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,6 +2,12 @@ import "./Cart.css";
 import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
 import { useCart } from "react-use-cart";
 import { CartProduct } from "../../components/CartProduct/CartProduct";
+import { Link } from "react-router-dom";
+import {
+  MingcuteCertificateFill,
+  MdiTruckCheck,
+  MingcuteSafetyCertificateFill,
+} from "../../components/Icons/Icons";
 
 export function Cart() {
   const { totalItems, items, cartTotal } = useCart();
@@ -28,8 +34,41 @@ export function Cart() {
         </div>
         <div className="cart_pay">
           <h3>Resumen del pedido </h3>
-          <p>Total $ {cartTotal} </p>
+          <hr />
+          <p>
+            Total <span>$ {cartTotal}</span>
+          </p>
+
           <button>Finalizar compra</button>
+          <Link
+            to={{
+              pathname: "/",
+              search: "?page=1&limit=20&search=&category=",
+            }}
+          >
+            Seguir comprando
+          </Link>
+          <hr />
+          <div className="cart_pay-info">
+            <div className="cart_pay-infoBox">
+              <span>
+                <MingcuteCertificateFill width={25} height={25} />
+              </span>
+              Calidad garantízada
+            </div>
+            <div className="cart_pay-infoBox">
+              <span>
+                <MdiTruckCheck width={25} height={25} />
+              </span>
+              Envío incluido
+            </div>
+            <div className="cart_pay-infoBox">
+              <span>
+                <MingcuteSafetyCertificateFill width={25} height={25} />
+              </span>
+              Compra 100% segura
+            </div>
+          </div>
         </div>
       </div>
     </section>
